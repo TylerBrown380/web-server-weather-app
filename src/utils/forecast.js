fcce625b95e3bca59e3d8ddfb725e153db5b38a5
Tyler Brown <Tylerbrown380@u.boisestate.edu>
2020-12-15 13:52:38 -0700

    fixed spacing

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         callback('Unable to find location.');
     } else {
        // console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ', but it feels like ' + response.body.current.feelslike + ' degrees out.');
-       callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' +body.current.temperature + 'degrees out, but it feels like ' + body.current.feelslike + ' degrees out.' + 
+       callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' +body.current.temperature + ' degrees out, but it feels like ' + body.current.feelslike + ' degrees out.' + 
        "The humidity out is currently " + body.current.humidity + "%.");
     }
 })
